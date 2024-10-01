e4e0592568daf92212d4c79bfea56b564da16f17
Imaniac230 <44968160+Imaniac230@users.noreply.github.com>
2024-10-01 13:19:01 +0200

    fix: Integrate the explicit frame prefix parameter - define robot name as optional to actually respect the specified usage.

--- spot_wrapper/wrapper.py
+++ spot_wrapper/wrapper.py
@@ -353,8 +353,9 @@ class SpotWrapper:
         password: str,
         hostname: str,
         port: int,
-        robot_name: str,
         logger: logging.Logger,
+        robot_name: typing.Optional[str] = None,
+        frame_prefix: typing.Optional[str] = None,
         start_estop: bool = True,
         estop_timeout: float = 9.0,
         rates: typing.Optional[typing.Dict[str, float]] = None,
@@ -373,6 +374,7 @@ class SpotWrapper:
             password: Password for authentication with the robot
             hostname: ip address or hostname of the robot
             robot_name: Optional name of the robot
+            frame_prefix: Optional prefix to be used for published message frames. If not provided, robot_name will be used to construct the prefix instead.
             start_estop: If true, the wrapper will be an estop endpoint
             estop_timeout: Timeout for the estop in seconds. The SDK will check in with the wrapper at a rate of
                            estop_timeout/3 and if there is no communication the robot will execute a gentle stop.
@@ -393,7 +395,6 @@ class SpotWrapper:
         self._password = password
         self._hostname = hostname
         self._payload_credentials_file = payload_credentials_file
-        self._robot_name = robot_name
         self._rates = rates or {}
         self._callbacks = callbacks or {}
         self._use_take_lease = use_take_lease
@@ -401,9 +402,8 @@ class SpotWrapper:
         self.decorate_functions()
         self._continually_try_stand = continually_try_stand
         self._rgb_cameras = rgb_cameras
-        self._frame_prefix = ""
-        if robot_name is not None:
-            self._frame_prefix = robot_name + "/"
+        self._robot_name = robot_name if robot_name is not None else ""
+        self._frame_prefix = frame_prefix if frame_prefix is not None else (robot_name + "/" if robot_name is not None else "")
         self._logger = logger
         self._estop_timeout = estop_timeout
         self._start_estop = start_estop
